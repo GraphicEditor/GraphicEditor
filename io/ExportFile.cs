a03377a029948960fe3c89892513d9360377edff
PavelKolotilin <BigPoulK@yandex.ru>
2024-02-29 23:25:46 +0700

    add ExportFile and converter file

--- io/ExportFile.cs
+++ io/ExportFile.cs
@@ -1,15 +1,52 @@
+﻿using System;
 using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
 
 namespace IO
 {
+    [Serializable]
+    public class BadFileExtensionError : Exception
+    {
+        public BadFileExtensionError(string message) : base(message)
+        { }
+
+        protected BadFileExtensionError(SerializationInfo info, StreamingContext ctxt) : base(info, ctxt)
+        { }
+    }
+
     public static class ExportFile
     {
-        public static void ToPNG(string filename, IDocument document)
+    
+        public static void ToSVG(string filename, object document)
         {
-            FileValidator.CheckParentDirectory(filename);
-            FileValidator.CheckExtension(filename, FileValidator.PNG_EXTENSION);
             var svgDoc = SVG.GetSvgDocumentFromDocument(document);
+            string svgString = SVG.GetStringFromSvgDocument(svgDoc);
+
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                writer.Write(svgString);
+            }
+        }
+
+
+        public static void ToPNG(string filename, object document)
+        {
+            var svgDoc = SVG.GetSvgDocumentFromDocument(document);
+
             svgDoc.Draw().Save(filename, ImageFormat.Png);
+        }
+
+        public static void ToPNGFromBitmap(string filename, Bitmap bitmap)
+        {
+            bitmap.Save(filename, ImageFormat.Png);
+        }
+
+        public static void ToJPEGFromBitmap(string filename, Bitmap bitmap)
+        {
+            bitmap.Save(filename, ImageFormat.Jpeg);
         }
     }
 }
