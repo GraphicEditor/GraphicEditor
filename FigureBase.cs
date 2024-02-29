9fc8ed663b830e97ccfa92af53411634f0daeff1
Егор <nedurilka@mail.ru>
2024-02-29 23:25:02 +0700

    Base of figures created succesfully

---
+++ FigureBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Windows;
+
+namespace Geometry
+{
+    public static class FigureBase
+    {
+        public static IFigure CreateRectangle()
+        {
+            return new Rectangle();
+        }
+        public static IFigure CreateTriangle()
+        {
+            return new Triangle();
+        }
+        public static IFigure CreatePolyline()
+        {
+            return new Polyline();
+        }
+    }
+    public interface IFigure : IAttachmentConsumer, IGeometricObject
+    {
+        IDictionary<vector> Parameters(get; set)
+    }
+}
