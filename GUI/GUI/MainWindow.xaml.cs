4ed4a544eb2fe1326cb0413f55e5628cc659b71c
Михаил Рояк <mikeroyak@gmail.com>
2024-03-01 16:21:08 +0700

    Example

--- GUI/GUI/MainWindow.xaml.cs
+++ GUI/GUI/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +23,11 @@ namespace GUI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello");
         }
     }
 }
