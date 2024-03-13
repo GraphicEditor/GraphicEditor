63df5d0df271c4dffd8a9a3cbd52ff3ff1a22a46
Егор <nedurilka@mail.ru>
2024-03-13 14:34:48 +0700

    the same fixes all over again

--- Geometry/FigureBase.cs
+++ Geometry/FigureBase.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,19 +12,53 @@ namespace Geometry
     {
         public static IFigure CreateRectangle()
         {
-            return new Rectangle();
+            throw new NotImplementedException();
+            //            return new Rectangle();
         }
         public static IFigure CreateTriangle()
         {
-            return new Triangle();
+            throw new NotImplementedException();
+            //           return new Triangle();
         }
         public static IFigure CreatePolyline()
         {
-            return new Polyline();
+            throw new NotImplementedException();
+            //            return new Polyline();
+        }
+        public static IFigure CreateCircle()
+        {
+            return new Circle();
         }
     }
-    public interface IFigure : IAttachmentConsumer, IGeometricObject
+    public interface IGraphicBase
     {
-        IDictionary<vector> Parameters(get; set)
+        void DrawLine(Point a, Point b);
+        void DrawCircle(Point center, float rad);
     }
+    public interface IFigure
+    {
+        IDictionary<string, Point> Parameters { get; }
+        Point Origin { get; }
+        bool IsInternal(Point p);
+        void Draw(IGraphicBase window);
+    }
+    public class Circle : IFigure
+    {
+        public IDictionary<string, Point> Parameters { get; } = new Dictionary<string, Point>()
+        {
+            ["Center"] = new Point(0, 0),
+            ["Point on circle"] = new Point(1, 0)
+        };
+        public Point Origin => Parameters["Center"];
+        double Radius => (Parameters["Point on circle"] - Origin).Length;
+        public bool IsInternal(Point p)
+        {
+            return (p - Origin).Length <= Radius;
+        }
+        public void Draw(IGraphicBase window)
+        {
+            window.DrawCircle(Origin, (float)Radius);
+        }
+    }
+
 }
