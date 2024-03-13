17ecc72b9bd0825431f583fbc808fb5a98065de1
Kostya <pinelis2206@mail.ru>
2024-03-13 14:24:47 +0700

    add circle

--- FigureBase.cs
+++ FigureBase.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,9 +22,40 @@ namespace Geometry
         {
             return new Polyline();
         }
+        public static IFigure CreateCircle()
+        {
+            return new Circle();
+        }
     }
-    public interface IFigure : IAttachmentConsumer, IGeometricObject
+    public interface IGraphicBase
     {
-        IDictionary<vector> Parameters(get; set)
+        void DrawLine(Point a, Point b);
+        void DrawCircle(Point center, float rad);
     }
+    public interface IFigure
+    {
+        IDictionary<string, Point> Parameters { get; }
+        Point Origin { get; }
+        bool IsInternal(Point p);
+        void Draw(IGraphicBase window);
+    }
+    public class Circle : IFigure
+    {
+        public IDictionary<string, Point> Parameters { get; } = new Dictionary<string, Point>()
+        {
+            ["Center"] = new Point(0, 0),
+            ["Point on circle"] = new Point(1, 0),
+        }
+        public Point Origin => Parameters["Center"];
+        double Radius => (Parameters["Point on circle"] - Origin).Lenght;
+        public bool IsInternal(Point p)
+        {
+            return (p - Origin).Norm <= Radius;
+        }
+        void Draw(IGraphicBase window)
+        {
+            window.DrawCircle(Origin, (float)Radius);
+        }
+    }
+
 }
